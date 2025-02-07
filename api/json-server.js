30b09d378703ec5bf9881d660f4b17dc1548767b
Motasem Ali <motasemali0000@gmail.com>
2025-02-07 21:07:23 +0200

    am done

--- api/json-server.js
+++ api/json-server.js
@@ -1,6 +1,8 @@
 const jsonServer = require("json-server");
+const path = require("path");
+
 const server = jsonServer.create();
-const router = jsonServer.router("/questions.json");
+const router = jsonServer.router(path.join(__dirname, "../questions.json")); // Correct path
 const middlewares = jsonServer.defaults();
 
 server.use(middlewares);
